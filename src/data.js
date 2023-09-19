df8a91115e2ca071a81e4c8d9e183918016e2ef6
ada-ip <ada7137@gmail.com>
2023-09-19 17:44:57 +0200

    First commit

---
+++ src/data.js
@@ -0,0 +1,75 @@
+const menu = [
+	{
+		id: 1,
+		title: "buttermilk pancakes",
+		category: "breakfast",
+		price: 11.99,
+		img: "/images/item-1.jpeg",
+		desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse porttitor mollis diam, et mollis massa viverra sollicitudin. Mauris efficitur lobortis diam. Curabitur at mauris non dui tempus consequat. Mauris quis quam eget velit sodales efficitur.`
+	},
+	{
+		id: 2,
+		title: "diner double",
+		category: "lunch",
+		price: 15.99,
+		img: "/images/item-2.jpeg",
+		desc: `Nunc faucibus efficitur felis, sed ultrices orci accumsan non. Sed semper velit nisl, ac faucibus lorem varius nec. Proin luctus lobortis erat, in finibus orci lacinia non.`
+	},
+	{
+		id: 3,
+		title: "godzilla milkshake",
+		category: "shakes",
+		price: 6.99,
+		img: "/images/item-3.jpeg",
+		desc: `In pretium, diam quis accumsan mollis, ligula turpis posuere turpis, ut hendrerit lacus est in lectus. Morbi vel orci nec ligula tincidunt interdum. Aenean id luctus risus, vitae eleifend dolor.`
+	},
+	{
+		id: 4,
+		title: "country delight",
+		category: "breakfast",
+		price: 9.99,
+		img: "/images/item-4.jpeg",
+		desc: `Suspendisse pretium ante sapien, vel ornare ipsum ornare sed. Etiam posuere risus sed cursus tincidunt. Aliquam venenatis quis augue et cursus.`
+	},
+	{
+		id: 5,
+		title: "egg attack",
+		category: "lunch",
+		price: 12.99,
+		img: "/images/item-5.jpeg",
+		desc: `Vivamus euismod magna et nibh pretium laoreet. Donec fermentum risus nec ullamcorper consequat. Integer at nulla aliquam, sagittis nisl in, sodales libero. Nam vestibulum accumsan nibh eu finibus. `
+	},
+	{
+		id: 6,
+		title: "oreo dream",
+		category: "shakes",
+		price: 5.99,
+		img: "/images/item-6.jpeg",
+		desc: `Sed fermentum velit arcu, at blandit lorem malesuada ut. Integer sed nibh vel ante varius consectetur vitae non dolor. Curabitur porttitor iaculis lorem, vel viverra enim euismod ut.`
+	},
+	{
+		id: 7,
+		title: "bacon overflow",
+		category: "breakfast",
+		price: 8.99,
+		img: "/images/item-7.jpeg",
+		desc: `Maecenas consectetur condimentum dolor in vehicula. Suspendisse aliquet, orci sagittis posuere lacinia, purus risus placerat purus, sit amet ultrices ante nisl quis lectus.`
+	},
+	{
+		id: 8,
+		title: "american classic",
+		category: "lunch",
+		price: 12.99,
+		img: "/images/item-8.jpeg",
+		desc: `Phasellus risus neque, ultrices eu tortor sed, molestie pharetra lectus. Cras sollicitudin tellus in vulputate rutrum. Sed gravida scelerisque ex nec lobortis.`
+	},
+	{
+		id: 9,
+		title: "great steak",
+		category: "lunch",
+		price: 14.99,
+		img: "/images/item-9.jpeg",
+		desc: `Duis blandit accumsan nibh, eget maximus metus consectetur vitae. Mauris cursus ex ac maximus fermentum. Nam quis lobortis magna. Vestibulum mauris lacus, suscipit ut venenatis posuere, cursus eget erat. `
+	}
+];
+export default menu;
