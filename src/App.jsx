e5b795b55aab55531e2342a7787e08a9658d2bb7
ada-ip <ada7137@gmail.com>
2023-09-20 13:07:11 +0200

    Add menu filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Title from "./components/Title.jsx";
+import TagsList from "./components/TagsList.jsx";
+import Menu from "./components/Menu.jsx";
+
+import menu from "./data";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [items, setItems] = useState(menu);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<main>
+			<Title />
+			<TagsList items={menu} setItems={setItems} />
+			<Menu items={items} />
+		</main>
+	);
 }
 
-export default App
+export default App;
--- src/components/TagsList.jsx
+++ src/components/TagsList.jsx
@@ -1,9 +1,26 @@
-const TagsList = ({ categories }) => {
+const TagsList = ({ items, setItems }) => {
+	const categories = items.reduce(
+		(listCategories, item) => {
+			if (!listCategories.includes(item.category)) {
+				listCategories.push(item.category);
+			}
+			return listCategories;
+		},
+		["all"]
+	);
+
+	const filterItems = (items, category) => {
+		if (category === "all") return items;
+		return items.filter((item) => item.category === category);
+	};
+
 	return (
 		<ul className="tags-list">
-			<li className="tag tags-list-tag">All</li>
 			{categories.map((category, index) => (
-				<li className="tag tags-list-tag" key={`cat${index}`}>
+				<li
+					className="tag tags-list-tag"
+					key={`cat${index}`}
+					onClick={(e) => setItems(filterItems(items, e.target.textContent))}>
 					{category}
 				</li>
 			))}
